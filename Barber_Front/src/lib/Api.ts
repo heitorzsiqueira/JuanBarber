c0026719cc633bdde869a246098543eea07a2fcd
Heitor Zeferino Siqueira <111435132+heitorzsiqueira@users.noreply.github.com>
2026-03-12 13:53:16 -0300

    Update Api.ts

--- Barber_Front/src/lib/Api.ts
+++ Barber_Front/src/lib/Api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { supabase } from './Supabase';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://juanbarber.onrender.com',
 });
 
 
