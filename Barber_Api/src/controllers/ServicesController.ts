1b6b0f24e8f1004657c4b9e6de7b3b3653663634
Heitor Zeferino Siqueira <111435132+heitorzsiqueira@users.noreply.github.com>
2026-03-26 10:36:11 -0300

    Improve error handling for service deletion

--- Barber_Api/src/controllers/ServicesController.ts
+++ Barber_Api/src/controllers/ServicesController.ts
@@ -28,7 +28,13 @@ export class ServicesController {
       await deleteServicesService.execute(service_id);
       return response.json({ message: "Serviço deletado com sucesso." });
     } catch (error) {
-      return response.status(400).json({ error: "Erro ao deletar serviço cuzinho." });
+     if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2003') {
+        return response.status(400).json({
+          error: "Este serviço já possui agendamentos registrados e não pode ser excluído para manter o histórico da barbearia."
+        });
+
+        return response.status(400).json({ error: "Erro ao deletar serviço cuzinho." + (error as Error).message });
+      }
     }
   }
 }
